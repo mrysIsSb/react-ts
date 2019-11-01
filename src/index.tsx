460097fec4868e0d059e646361aaca613e2825f7
mrys <1510218499@qq.com>
2019-11-01 17:54:15 +0800

    css

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Page from "./Page";
+import * as apis from './axios';
+import './style/lib/animate.css';
+import './style/antd/index.less';
+import './style/index.less';
 ReactDOM.render(<Page />, document.getElementById("root"));
